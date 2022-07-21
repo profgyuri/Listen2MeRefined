71ee26fb6c74a5ba488b7b22302fd1526a529f78
Turbék György <gyorgy.turbek@gmail.com>
2022-07-22 01:05:55 +0200

    Added ui utility code

---
+++ Listen2MeRefined.WPF/SmoothScrollViewer.cs
@@ -0,0 +1,319 @@
+﻿namespace Listen2MeRefined.WPF;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+[TemplatePart(Name = "PART_AniVerticalScrollBar", Type = typeof(ScrollBar))]
+[TemplatePart(Name = "PART_AniHorizontalScrollBar", Type = typeof(ScrollBar))]
+public class SmoothScrollViewer : ScrollViewer
+{
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    private ScrollBar _aniHorizontalScrollBar;
+    private ScrollBar _aniVerticalScrollBar;
+
+    public static readonly DependencyProperty _targetVerticalOffsetProperty =
+        DependencyProperty.Register("TargetVerticalOffset", typeof(double), typeof(SmoothScrollViewer),
+            new PropertyMetadata(0.0, OnTargetVerticalOffsetChanged));
+
+    public static readonly DependencyProperty _targetHorizontalOffsetProperty =
+        DependencyProperty.Register("TargetHorizontalOffset", typeof(double), typeof(SmoothScrollViewer),
+            new PropertyMetadata(0.0, OnTargetHorizontalOffsetChanged));
+
+    public static readonly DependencyProperty _horizontalScrollOffsetProperty =
+        DependencyProperty.Register("HorizontalScrollOffset", typeof(double), typeof(SmoothScrollViewer),
+            new PropertyMetadata(0.0, OnHorizontalScrollOffsetChanged));
+
+    public static readonly DependencyProperty _verticalScrollOffsetProperty =
+        DependencyProperty.Register("VerticalScrollOffset", typeof(double), typeof(SmoothScrollViewer),
+            new PropertyMetadata(0.0, OnVerticalScrollOffsetChanged));
+
+    public static readonly DependencyProperty _scrollingTimeProperty =
+        DependencyProperty.Register("ScrollingTime", typeof(TimeSpan), typeof(SmoothScrollViewer),
+            new PropertyMetadata(new TimeSpan(0, 0, 0,
+                0, 500)));
+
+    public static readonly DependencyProperty _scrollingSplineProperty =
+        DependencyProperty.Register("ScrollingSpline", typeof(KeySpline), typeof(SmoothScrollViewer),
+            new PropertyMetadata(new KeySpline(0.024, 0.914, 0.717,
+                1)));
+
+    public static readonly DependencyProperty _canKeyboardScrollProperty =
+        DependencyProperty.Register("CanKeyboardScroll", typeof(bool), typeof(SmoothScrollViewer),
+            new FrameworkPropertyMetadata(true));
+
+    /// <summary>
+    ///     This is the VerticalOffset that we'd like to animate to.
+    /// </summary>
+    private double TargetVerticalOffset
+    {
+        get => (double)GetValue(_targetVerticalOffsetProperty);
+        set => SetValue(_targetVerticalOffsetProperty, value);
+    }
+
+    /// <summary>
+    ///     This is the HorizontalOffset that we'll be animating to.
+    /// </summary>
+    private double TargetHorizontalOffset
+    {
+        get => (double)GetValue(_targetHorizontalOffsetProperty);
+        set => SetValue(_targetHorizontalOffsetProperty, value);
+    }
+
+    /// <summary>
+    ///     A property for changing the time it takes to scroll to a new position.
+    /// </summary>
+    private TimeSpan ScrollingTime => (TimeSpan)GetValue(_scrollingTimeProperty);
+
+    /// <summary>
+    ///     A property to allow users to describe a custom spline for the scrolling animation.
+    /// </summary>
+    private KeySpline ScrollingSpline => (KeySpline)GetValue(_scrollingSplineProperty);
+
+    private bool CanKeyboardScroll => (bool)GetValue(_canKeyboardScrollProperty);
+
+    static SmoothScrollViewer()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(SmoothScrollViewer),
+            new FrameworkPropertyMetadata(typeof(SmoothScrollViewer)));
+    }
+
+    public override void OnApplyTemplate()
+    {
+        base.OnApplyTemplate();
+
+        if (GetTemplateChild("PART_AniVerticalScrollBar") is ScrollBar aniVScroll)
+        {
+            _aniVerticalScrollBar = aniVScroll;
+        }
+
+        _aniVerticalScrollBar.ValueChanged += VScrollBar_ValueChanged;
+
+        if (GetTemplateChild("PART_AniHorizontalScrollBar") is ScrollBar aniHScroll)
+        {
+            _aniHorizontalScrollBar = aniHScroll;
+        }
+
+        _aniHorizontalScrollBar.ValueChanged += HScrollBar_ValueChanged;
+
+        PreviewMouseWheel += CustomPreviewMouseWheel;
+        PreviewKeyDown += AnimatedScrollViewer_PreviewKeyDown;
+    }
+
+    private static void AnimatedScrollViewer_PreviewKeyDown(object sender, KeyEventArgs e)
+    {
+        var thisScroller = (SmoothScrollViewer)sender;
+
+        if (!thisScroller.CanKeyboardScroll)
+        {
+            return;
+        }
+
+        var keyPressed = e.Key;
+        var newVerticalPos = thisScroller.TargetVerticalOffset;
+        var newHorizontalPos = thisScroller.TargetHorizontalOffset;
+        bool isKeyHandled;
+
+        // ReSharper disable once SwitchStatementHandlesSomeKnownEnumValuesWithDefault
+        switch (keyPressed)
+        {
+            //Vertical Key Strokes code
+            case Key.Down:
+                newVerticalPos = NormalizeScrollPos(thisScroller, newVerticalPos + 16.0, Orientation.Vertical);
+                isKeyHandled = true;
+                break;
+            case Key.PageDown:
+                newVerticalPos = NormalizeScrollPos(thisScroller, newVerticalPos + thisScroller.ViewportHeight,
+                    Orientation.Vertical);
+                isKeyHandled = true;
+                break;
+            case Key.Up:
+                newVerticalPos = NormalizeScrollPos(thisScroller, newVerticalPos - 16.0, Orientation.Vertical);
+                isKeyHandled = true;
+                break;
+            case Key.PageUp:
+                newVerticalPos = NormalizeScrollPos(thisScroller, newVerticalPos - thisScroller.ViewportHeight,
+                    Orientation.Vertical);
+                isKeyHandled = true;
+                break;
+            default:
+#pragma warning disable CA2208
+                throw new ArgumentOutOfRangeException();
+#pragma warning restore CA2208
+        }
+
+        if (Math.Abs(newVerticalPos - thisScroller.TargetVerticalOffset) > 0.0F)
+        {
+            thisScroller.TargetVerticalOffset = newVerticalPos;
+        }
+
+        //Horizontal Key Strokes Code
+        if (keyPressed == Key.Right)
+        {
+            newHorizontalPos = NormalizeScrollPos(thisScroller, newHorizontalPos + 16, Orientation.Horizontal);
+            isKeyHandled = true;
+        }
+        else if (keyPressed == Key.Left)
+        {
+            newHorizontalPos = NormalizeScrollPos(thisScroller, newHorizontalPos - 16, Orientation.Horizontal);
+            isKeyHandled = true;
+        }
+
+        if (Math.Abs(newHorizontalPos - thisScroller.TargetHorizontalOffset) > 0.0F)
+        {
+            thisScroller.TargetHorizontalOffset = newHorizontalPos;
+        }
+
+        e.Handled = isKeyHandled;
+    }
+
+    private static double NormalizeScrollPos(ScrollViewer thisScroll, double scrollChange, Orientation o)
+    {
+        var returnValue = scrollChange;
+
+        if (scrollChange < 0)
+        {
+            returnValue = 0;
+        }
+
+        if (o == Orientation.Vertical && scrollChange > thisScroll.ScrollableHeight)
+        {
+            return thisScroll.ScrollableHeight;
+        }
+
+        if (o == Orientation.Horizontal && scrollChange > thisScroll.ScrollableWidth)
+        {
+            return thisScroll.ScrollableWidth;
+        }
+
+        return returnValue;
+    }
+
+    private static void CustomPreviewMouseWheel(object sender, MouseWheelEventArgs e)
+    {
+        double mouseWheelChange = e.Delta;
+
+        var thisScroller = (SmoothScrollViewer)sender;
+        var newVOffset = thisScroller.TargetVerticalOffset - mouseWheelChange / 3;
+
+        if (newVOffset < 0)
+        {
+            thisScroller.TargetVerticalOffset = 0;
+        }
+        else if (newVOffset > thisScroller.ScrollableHeight)
+        {
+            thisScroller.TargetVerticalOffset = thisScroller.ScrollableHeight;
+        }
+        else
+        {
+            thisScroller.TargetVerticalOffset = newVOffset;
+        }
+
+        e.Handled = true;
+    }
+
+    private void VScrollBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+        var thisScroller = this;
+        var oldTargetVOffset = e.OldValue;
+        var newTargetVOffset = e.NewValue;
+
+        if (!(Math.Abs(newTargetVOffset - thisScroller.TargetVerticalOffset) > 0.0F))
+        {
+            return;
+        }
+
+        var deltaVOffset = Math.Round(newTargetVOffset - oldTargetVOffset, 3);
+
+        thisScroller.TargetVerticalOffset =
+            deltaVOffset switch
+            {
+                1 => oldTargetVOffset + thisScroller.ViewportWidth,
+                -1 => oldTargetVOffset - thisScroller.ViewportWidth,
+                0.1 => oldTargetVOffset + 16.0,
+                -0.1 => oldTargetVOffset - 16.0,
+                _ => newTargetVOffset
+            };
+    }
+
+    private void HScrollBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+        var thisScroller = this;
+
+        var oldTargetHOffset = e.OldValue;
+        var newTargetHOffset = e.NewValue;
+
+        if (!(Math.Abs(newTargetHOffset - thisScroller.TargetHorizontalOffset) > 0.0F))
+        {
+            return;
+        }
+
+        var deltaVOffset = Math.Round(newTargetHOffset - oldTargetHOffset, 3);
+
+        thisScroller.TargetHorizontalOffset =
+            deltaVOffset switch
+            {
+                1 => oldTargetHOffset + thisScroller.ViewportWidth,
+                -1 => oldTargetHOffset - thisScroller.ViewportWidth,
+                0.1 => oldTargetHOffset + 16.0,
+                -0.1 => oldTargetHOffset - 16.0,
+                _ => newTargetHOffset
+            };
+    }
+
+    private static void OnTargetVerticalOffsetChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var thisScroller = (SmoothScrollViewer)d;
+
+        if (Math.Abs((double)e.NewValue - thisScroller._aniVerticalScrollBar.Value) > 0.0D)
+        {
+            thisScroller._aniVerticalScrollBar.Value = (double)e.NewValue;
+        }
+
+        AnimateScroller(thisScroller);
+    }
+
+    private static void OnTargetHorizontalOffsetChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var thisScroller = (SmoothScrollViewer)d;
+
+        if (Math.Abs((double)e.NewValue - thisScroller._aniHorizontalScrollBar.Value) > 0.0D)
+        {
+            thisScroller._aniHorizontalScrollBar.Value = (double)e.NewValue;
+        }
+
+        AnimateScroller(thisScroller);
+    }
+
+    private static void OnHorizontalScrollOffsetChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var thisSViewer = (SmoothScrollViewer)d;
+        thisSViewer.ScrollToHorizontalOffset((double)e.NewValue);
+    }
+
+    private static void OnVerticalScrollOffsetChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var thisSViewer = (SmoothScrollViewer)d;
+        thisSViewer.ScrollToVerticalOffset((double)e.NewValue);
+    }
+
+    private static void AnimateScroller(object objectToScroll)
+    {
+        var thisScrollViewer = objectToScroll as SmoothScrollViewer;
+
+        KeyTime targetKeyTime = thisScrollViewer!.ScrollingTime;
+        var targetKeySpline = thisScrollViewer.ScrollingSpline;
+
+        DoubleAnimationUsingKeyFrames animateHScrollKeyFramed = new();
+        DoubleAnimationUsingKeyFrames animateVScrollKeyFramed = new();
+
+        SplineDoubleKeyFrame hScrollKey1 = new(thisScrollViewer.TargetHorizontalOffset, targetKeyTime, targetKeySpline);
+        SplineDoubleKeyFrame vScrollKey1 = new(thisScrollViewer.TargetVerticalOffset, targetKeyTime, targetKeySpline);
+        animateHScrollKeyFramed.KeyFrames.Add(hScrollKey1);
+        animateVScrollKeyFramed.KeyFrames.Add(vScrollKey1);
+
+        thisScrollViewer.BeginAnimation(_horizontalScrollOffsetProperty, animateHScrollKeyFramed);
+        thisScrollViewer.BeginAnimation(_verticalScrollOffsetProperty, animateVScrollKeyFramed);
+    }
+}
---
+++ Listen2MeRefined.WPF/WindowResizer.cs
@@ -0,0 +1,163 @@
+﻿namespace Listen2MeRefined.WPF;
+
+using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+
+/// <summary>
+///     Fixes the issue with Windows of Style <see cref="WindowStyle.None" /> covering the taskbar. Do not use MaxWidth and
+///     MaxHeight!
+/// </summary>
+public class WindowResizer
+{
+    /// <summary>
+    ///     The window to handle the resizing for
+    /// </summary>
+    private readonly Window _mWindow;
+
+    /// <summary>
+    ///     Default constructor
+    /// </summary>
+    /// <param name="window">The window to monitor and correctly maximize</param>
+    public WindowResizer(Window window)
+    {
+        _mWindow = window;
+
+        // Listen out for source initialized to setup
+        _mWindow.SourceInitialized += Window_SourceInitialized;
+    }
+
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool GetCursorPos(out Point lpPoint);
+
+    [DllImport("user32.dll")]
+    private static extern bool GetMonitorInfo(IntPtr hMonitor, MonitorInfo lpmi);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern IntPtr MonitorFromPoint(Point pt, MonitorOptions dwFlags);
+
+    /// <summary>
+    ///     Initialize and hook into the windows message pump
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void Window_SourceInitialized(object? sender, EventArgs e)
+    {
+        // Get the handle of this window
+        var handle = new WindowInteropHelper(_mWindow).Handle;
+        var handleSource = HwndSource.FromHwnd(handle);
+
+        // Hook into it's Windows messages
+        handleSource?.AddHook(WindowProc);
+    }
+
+    /// <summary>
+    ///     Listens out for all windows messages for this window
+    /// </summary>
+    /// <param name="hwnd"></param>
+    /// <param name="msg"></param>
+    /// <param name="wParam"></param>
+    /// <param name="lParam"></param>
+    /// <param name="handled"></param>
+    /// <returns></returns>
+    private static IntPtr WindowProc(IntPtr hwnd, int msg, IntPtr wParam,
+        IntPtr lParam, ref bool handled)
+    {
+        switch (msg)
+        {
+            // Handle the GetMinMaxInfo of the Window
+            case 0x0024: /* WM_GETMINMAXINFO */
+                WmGetMinMaxInfo(lParam);
+                handled = true;
+                break;
+        }
+
+        return (IntPtr)0;
+    }
+
+    /// <summary>
+    ///     Get the min/max window size for this window
+    ///     Correctly accounting for the taskbar size and position
+    /// </summary>
+    /// <param name="lParam"></param>
+    private static void WmGetMinMaxInfo(IntPtr lParam)
+    {
+        GetCursorPos(out var lMousePosition);
+
+        var lPrimaryScreen = MonitorFromPoint(new Point(0, 0), MonitorOptions.MonitorDefaulttoprimary);
+        MonitorInfo lPrimaryScreenInfo = new();
+        if (!GetMonitorInfo(lPrimaryScreen, lPrimaryScreenInfo))
+        {
+            return;
+        }
+
+        var lCurrentScreen = MonitorFromPoint(lMousePosition, MonitorOptions.MonitorDefaulttonearest);
+
+        var lMmi = (MinMaxInfo)Marshal.PtrToStructure(lParam, typeof(MinMaxInfo))!;
+
+        if (lPrimaryScreen.Equals(lCurrentScreen))
+        {
+            lMmi.ptMaxPosition.X = lPrimaryScreenInfo.rcWork.Left;
+            lMmi.ptMaxPosition.Y = lPrimaryScreenInfo.rcWork.Top;
+            lMmi.ptMaxSize.X = lPrimaryScreenInfo.rcWork.Right - lPrimaryScreenInfo.rcWork.Left;
+            lMmi.ptMaxSize.Y = lPrimaryScreenInfo.rcWork.Bottom - lPrimaryScreenInfo.rcWork.Top;
+        }
+        else
+        {
+            lMmi.ptMaxPosition.X = lPrimaryScreenInfo.rcMonitor.Left;
+            lMmi.ptMaxPosition.Y = lPrimaryScreenInfo.rcMonitor.Top;
+            lMmi.ptMaxSize.X = lPrimaryScreenInfo.rcMonitor.Right - lPrimaryScreenInfo.rcMonitor.Left;
+            lMmi.ptMaxSize.Y = lPrimaryScreenInfo.rcMonitor.Bottom - lPrimaryScreenInfo.rcMonitor.Top;
+        }
+
+        // Now we have the max size, allow the host to tweak as needed
+        Marshal.StructureToPtr(lMmi, lParam, true);
+    }
+}
+
+public enum MonitorOptions : uint
+{
+    MonitorDefaulttoprimary = 0x00000001,
+    MonitorDefaulttonearest = 0x00000002
+}
+
+[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+public class MonitorInfo
+{
+    public Rectangle rcMonitor = new();
+    public Rectangle rcWork = new();
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct Rectangle
+{
+    public readonly int Left;
+    public readonly int Top;
+    public readonly int Right;
+    public readonly int Bottom;
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct MinMaxInfo
+{
+    private readonly Point ptReserved;
+    public Point ptMaxSize;
+    public Point ptMaxPosition;
+    private readonly Point ptMinTrackSize;
+    private readonly Point ptMaxTrackSize;
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct Point
+{
+    public int X;
+    public int Y;
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
