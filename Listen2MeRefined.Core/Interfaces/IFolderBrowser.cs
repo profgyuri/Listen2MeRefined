5476bc0d826ada3f6a5d5b6048e24c5c17052b97
Turbék György <gyorgy.turbek@gmail.com>
2022-08-03 00:50:06 +0200

    Started working on folder browser dialog backend

---
+++ Listen2MeRefined.Core/Interfaces/IFolderBrowser.cs
@@ -0,0 +1,17 @@
+﻿namespace Listen2MeRefined.Core.Interfaces;
+
+public interface IFolderBrowser
+{
+    /// <summary>
+    ///     Returns every subfolder in <paramref name="path"/>.
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    IEnumerable<string> GetSubFolders(string path);
+
+    /// <summary>
+    ///     Returns a list of the phisycal drives.
+    /// </summary>
+    /// <returns></returns>
+    IEnumerable<string> GetDrives();
+}
---
+++ Listen2MeRefined.Infrastructure/Mvvm/FolderBrowserViewModel.cs
@@ -0,0 +1,36 @@
+﻿namespace Listen2MeRefined.Infrastructure.Mvvm;
+
+using System.Collections.ObjectModel;
+
+[INotifyPropertyChanged]
+public partial class FolderBrowserViewModel
+{
+    private readonly ILogger _logger;
+    private readonly IFolderBrowser _folderBrowser;
+
+    [ObservableProperty] private string _fullPath = "";
+    [ObservableProperty] private string _selectedFolder = "";
+    [ObservableProperty] private ObservableCollection<string> _folders = new();
+
+    public FolderBrowserViewModel(ILogger logger, IFolderBrowser folderBrowser)
+    {
+        _logger = logger;
+        _folderBrowser = folderBrowser;
+    }
+
+    [ICommand]
+    private void ChangeDirectory()
+    {
+        
+    }
+
+    /// <summary>
+    ///     This method should be called when the browsing is done, 
+    ///     and we have a selected path.
+    /// </summary>
+    [ICommand]
+    private void HandleSelectedPath()
+    {
+        
+    }
+}
