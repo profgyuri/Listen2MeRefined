991e6b37b5334df371c833c43db519214daf92d7
Turbék György <gyorgy.turbek@gmail.com>
2022-10-08 02:41:38 +0200

    Refactored WindowsMusicPlayer.cs and added an extra failsafe for song skipping

--- Listen2MeRefined.Infrastructure/Media/WindowsMusicPlayer.cs
+++ Listen2MeRefined.Infrastructure/Media/WindowsMusicPlayer.cs
@@ -3,11 +3,10 @@ using MediatR;
 using Source;
 using Source.Extensions;
 using Source.KeyboardHook;
-
-namespace Listen2MeRefined.Infrastructure.Media;
-
 using NAudio.Wave;
 using System.Collections.ObjectModel;
+
+namespace Listen2MeRefined.Infrastructure.Media;
 
 /// <summary>
 ///     Wrapper class for NAudio.
@@ -15,8 +14,9 @@ using System.Collections.ObjectModel;
 public sealed class WindowsMusicPlayer : IMediaController, IPlaylistReference
 {
     private bool _startSongAutomatically;
-    private int _playlistIndex;
+    private int _currentSongIndex;
     private double _previousTimeStamp;
+    private double _unpausedFor;
     private AudioModel? _currentSong;
     private AudioFileReader? _fileReader;
     private WaveOutEvent _waveOutEvent = new();
@@ -26,6 +26,8 @@ public sealed class WindowsMusicPlayer : IMediaController, IPlaylistReference
     private readonly ILogger _logger;
     private readonly IMediator _mediator;
     private readonly IRepository<AudioModel> _audioRepository;
+    
+    private const int TimeCheckInterval = 500;
 
     public double CurrentTime
     {
@@ -54,7 +56,7 @@ public sealed class WindowsMusicPlayer : IMediaController, IPlaylistReference
         _mediator = mediator;
         _audioRepository = audioRepository;
 
-        timedTask.Start(TimeSpan.FromMilliseconds(500), CurrentTimeCheck);
+        timedTask.Start(TimeSpan.FromMilliseconds(TimeCheckInterval), CurrentTimeCheck);
         keyboardHook.KeyboardPressed += KeyboardPressedEvent;
     }
 
@@ -63,24 +65,11 @@ public sealed class WindowsMusicPlayer : IMediaController, IPlaylistReference
     {
         if (_playbackState == PlaybackState.Playing)
         {
-            _waveOutEvent.Pause();
-            _startSongAutomatically = false;
-            _playbackState = PlaybackState.Paused;
-
-            _logger.Debug("Playback paused");
-
+            PausePlayback();
             return;
         }
-        
-        _playbackState = PlaybackState.Playing;
 
-        if (_currentSong is null && _playlist.Any())
-        {
-            LoadCurrentSong();
-        }
-
-        _startSongAutomatically = true;
-        _waveOutEvent.Play();
+        StartPlayback();
     }
 
     public void Stop()
@@ -100,27 +89,27 @@ public sealed class WindowsMusicPlayer : IMediaController, IPlaylistReference
 
     public void Next()
     {
-        _playlistIndex = (_playlistIndex + 1) % _playlist!.Count;
+        _currentSongIndex = (_currentSongIndex + 1) % _playlist!.Count;
 
         LoadCurrentSong();
 
         _logger.Debug("Jumping to the next song at index {Current} with the maximum possible index of {Maximum}...",
-            _playlistIndex, _playlist.Count - 1);
+            _currentSongIndex, _playlist.Count - 1);
     }
 
     public void Previous()
     {
-        _playlistIndex = (_playlistIndex - 1 + _playlist!.Count) % _playlist.Count;
+        _currentSongIndex = (_currentSongIndex - 1 + _playlist!.Count) % _playlist.Count;
 
         LoadCurrentSong();
 
         _logger.Debug("Jumping to the previous song at index {Current} with the maximum possible index of {Maximum}...",
-            _playlistIndex, _playlist.Count - 1);
+            _currentSongIndex, _playlist.Count - 1);
     }
     
     public void JumpToIndex(int index)
     {
-        _playlistIndex = index;
+        _currentSongIndex = index;
         LoadCurrentSong();
     }
 
@@ -132,12 +121,11 @@ public sealed class WindowsMusicPlayer : IMediaController, IPlaylistReference
         
         if (_currentSong is not null)
         {
-            // next 2 lines will put the current song to the 1st place
-            var index = _playlist.IndexOf(_currentSong);
-            (_playlist[0], _playlist[index]) = (_playlist[index], _playlist[0]);
+            (_playlist[0], _playlist[_currentSongIndex]) = 
+                (_playlist[_currentSongIndex], _playlist[0]);
         }
         
-        _playlistIndex = 0;
+        _currentSongIndex = 0;
     }
     #endregion
 
@@ -145,7 +133,7 @@ public sealed class WindowsMusicPlayer : IMediaController, IPlaylistReference
     private void LoadCurrentSong()
     {
         _logger.Information("Loading audio: {Song}", _currentSong);
-        _currentSong = _playlist[_playlistIndex];
+        _currentSong = _playlist[_currentSongIndex];
 
         try
         {
@@ -164,6 +152,7 @@ public sealed class WindowsMusicPlayer : IMediaController, IPlaylistReference
         catch (Exception ex)
         {
             _logger.Error(ex, "An unexpected error occured!");
+            throw;
         }
         
         _mediator.Publish(new CurrentSongNotification(_currentSong));
@@ -171,6 +160,7 @@ public sealed class WindowsMusicPlayer : IMediaController, IPlaylistReference
         ReNewWaveOutEvent();
 
         _previousTimeStamp = -1;
+        _unpausedFor = 0;
 
         if (_startSongAutomatically)
         {
@@ -196,22 +186,44 @@ public sealed class WindowsMusicPlayer : IMediaController, IPlaylistReference
 
     private void CurrentTimeCheck()
     {
-        if (_fileReader is null)
+        if (_fileReader is null
+            || _playbackState is not PlaybackState.Playing)
         {
             return;
         }
 
         if (Math.Abs(_fileReader.CurrentTime.TotalMilliseconds - _previousTimeStamp) < 0.1
-            && _playbackState == PlaybackState.Playing
-            && _previousTimeStamp >= 300)
+            && _previousTimeStamp >= TimeCheckInterval
+            && _unpausedFor > TimeCheckInterval)
         {
             Next();
         }
 
-        if (_playbackState is PlaybackState.Playing)
+        _previousTimeStamp = _fileReader.CurrentTime.TotalMilliseconds;
+        _unpausedFor += TimeCheckInterval;
+    }
+    
+    private void StartPlayback()
+    {
+        _playbackState = PlaybackState.Playing;
+        _unpausedFor = 0;
+
+        if (_currentSong is null && _playlist.Any())
         {
-            _previousTimeStamp = _fileReader.CurrentTime.TotalMilliseconds;
+            LoadCurrentSong();
         }
+
+        _startSongAutomatically = true;
+        _waveOutEvent.Play();
+    }
+
+    private void PausePlayback()
+    {
+        _waveOutEvent.Pause();
+        _startSongAutomatically = false;
+        _playbackState = PlaybackState.Paused;
+
+        _logger.Debug("Playback paused");
     }
     #endregion
 
