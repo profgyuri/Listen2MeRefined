88da49e2319e8cbd1be49c6441113a0e6f018de2
Turbék György <gyorgy.turbek@gmail.com>
2022-11-03 17:50:33 +0100

    Added notification handler for font family change in new song window

--- Listen2MeRefined.Infrastructure/Mvvm/NewSongWindowViewModel.cs
+++ Listen2MeRefined.Infrastructure/Mvvm/NewSongWindowViewModel.cs
@@ -4,16 +4,27 @@ using MediatR;
 namespace Listen2MeRefined.Infrastructure.Mvvm;
 
 [INotifyPropertyChanged]
-public partial class NewSongWindowViewModel
-    : INotificationHandler<CurrentSongNotification>
+public partial class NewSongWindowViewModel : 
+    INotificationHandler<CurrentSongNotification>,
+    INotificationHandler<FontFamilyChangedNotification>
 {
     [ObservableProperty] private AudioModel _song = new();
+    [ObservableProperty] private string _fontFamily = default!;
 
     #region Implementation of INotificationHandler<in CurrentSongNotification>
     /// <inheritdoc />
     public Task Handle(CurrentSongNotification notification, CancellationToken cancellationToken)
     {
         Song = notification.Audio;
+        return Task.CompletedTask;
+    }
+    #endregion
+
+    #region Implementation of INotificationHandler<in FontFamilyChangedNotification>
+    /// <inheritdoc />
+    public Task Handle(FontFamilyChangedNotification notification, CancellationToken cancellationToken)
+    {
+        FontFamily = notification.FontFamily;
         return Task.CompletedTask;
     }
     #endregion
