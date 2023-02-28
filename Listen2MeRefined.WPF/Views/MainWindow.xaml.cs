38391aa71dd94b9efc52d81cf4c59c86c1135307
György Turbék <profgyuri@gmail.com>
2023-02-28 01:30:28 +0100

    Added code for the swiping text animation

--- Listen2MeRefined.WPF/Views/MainWindow.xaml.cs
+++ Listen2MeRefined.WPF/Views/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Media.Animation;
 using Listen2MeRefined.WPF.Views;
 using Listen2MeRefined.WPF.Views.Pages;
 
@@ -10,6 +12,9 @@ namespace Listen2MeRefined.WPF;
 /// </summary>
 public sealed partial class MainWindow : Window
 {
+    private readonly KeySpline _easeOutKeySpline = new(0.5, 0, 0.5, 1);
+    private readonly Dictionary<string, Storyboard> _storyboards = new();
+
     public MainWindow(
         MainWindowViewModel viewModel,
         CurrentlyPlayingPage currentlyPlayingPage)
@@ -64,4 +69,116 @@ public sealed partial class MainWindow : Window
         var vm = (MainWindowViewModel)DataContext;
         vm.RefreshSoundWave().ConfigureAwait(false);
     }
+
+    #region Flowing text animation
+    private void DisplayText_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+    {
+        var flowingTextBlock = (sender as StoryboardTextBlock)!;
+
+        //if the storyboard is already playing, just return
+        if (_storyboards.ContainsKey(flowingTextBlock.StoryboardName))
+        {
+            return;
+        }
+
+        //we retrieve the full width of the text which is bound to the tag property
+        var fullWidth = (double)flowingTextBlock.Tag;
+        double swipeAmount = fullWidth - flowingTextBlock.ActualWidth;
+
+        //getting a smooth amount of swipe duration for longer texts
+        var swipeSeconds = swipeAmount / 100 * 2d;
+
+        //setting a minimum animation duration, so short texts dont just "jump" around
+        swipeSeconds = swipeSeconds >= 3 ? swipeSeconds : 3;
+
+        if (swipeAmount <= 0)
+        {
+            return;
+        }
+
+        var thicknessAnimation = new ThicknessAnimationUsingKeyFrames()
+        {
+            RepeatBehavior = new RepeatBehavior(1),
+            AutoReverse = true
+        };
+        thicknessAnimation.KeyFrames.Add(
+            new SplineThicknessKeyFrame(
+                new Thickness(0, 0, 0, 0),
+                KeyTime.FromTimeSpan(TimeSpan.FromSeconds(.5))));
+
+        if (swipeAmount > 0)
+        {
+            thicknessAnimation.KeyFrames.Add(
+                new SplineThicknessKeyFrame(
+                    new Thickness(-swipeAmount, 0, 0, 0),
+                    KeyTime.FromTimeSpan(TimeSpan.FromSeconds(swipeSeconds)),
+                    _easeOutKeySpline));
+
+            thicknessAnimation.KeyFrames.Add(
+                new SplineThicknessKeyFrame(
+                    new Thickness(-swipeAmount, 0, 0, 0),
+                    KeyTime.FromTimeSpan(TimeSpan.FromSeconds(swipeSeconds + .5)),
+                    _easeOutKeySpline));
+        }
+
+        Storyboard.SetTarget(thicknessAnimation, flowingTextBlock);
+        Storyboard.SetTargetProperty(thicknessAnimation, new PropertyPath("(TextBlock.Margin)"));
+
+        var storyboard = new Storyboard();
+        var name = $"a{Guid.NewGuid():N}";
+        storyboard.Name = name;
+        flowingTextBlock.StoryboardName = name;
+        storyboard.Children.Add(thicknessAnimation);
+        storyboard.Begin();
+        _storyboards.Add(storyboard.Name, storyboard);
+    }
+
+    private void DisplayText_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+    {
+        var FlowingTetxBlock = (sender as StoryboardTextBlock)!;
+
+        if (!_storyboards.ContainsKey(FlowingTetxBlock.StoryboardName))
+        {
+            return;
+        }
+
+        double swipeAmount = FlowingTetxBlock.Margin.Left;
+
+        //getting a smooth amount of swipe duration for longer texts
+        var swipeSeconds = Math.Abs(swipeAmount / 100 * 1.33d);
+        swipeSeconds = swipeSeconds >= 1 ? swipeSeconds : 1;
+
+        var thicknessAnimation = new ThicknessAnimationUsingKeyFrames()
+        {
+            RepeatBehavior = new RepeatBehavior(1)
+        };
+
+        if (Math.Abs(swipeAmount) > 0)
+        {
+            thicknessAnimation.KeyFrames.Add(
+                new SplineThicknessKeyFrame(
+                    new Thickness(swipeAmount, 0, 0, 0),
+                    KeyTime.FromTimeSpan(TimeSpan.FromSeconds(0)),
+                    _easeOutKeySpline));
+
+            thicknessAnimation.KeyFrames.Add(
+               new SplineThicknessKeyFrame(
+                   new Thickness(0, 0, 0, 0),
+                   KeyTime.FromTimeSpan(TimeSpan.FromSeconds(swipeSeconds)),
+                   _easeOutKeySpline));
+        }
+
+        Storyboard.SetTarget(thicknessAnimation, FlowingTetxBlock);
+        Storyboard.SetTargetProperty(thicknessAnimation, new PropertyPath("(TextBlock.Margin)"));
+
+        var name = FlowingTetxBlock.StoryboardName;
+        var storyboard = _storyboards[name];
+        storyboard.Stop();
+        storyboard = new();
+        storyboard.Name = name;
+        storyboard.Children.Add(thicknessAnimation);
+        storyboard.Completed += (s, e) => _storyboards.Remove(storyboard.Name);
+        storyboard.Begin();
+    }
+    #endregion
 }
