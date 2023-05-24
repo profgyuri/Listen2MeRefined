ea292f5c2c99d0516184507004c6dc05334147ae
György Turbék <profgyuri@gmail.com>
2023-05-25 00:22:25 +0200

    Added custom rating bar

---
+++ Listen2MeRefined.WPF/Resources/RatingBar.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Listen2MeRefined.WPF.Resources;
+
+/// <summary>
+/// Interaction logic for RatingBar.xaml
+/// </summary>
+public partial class RatingBar : UserControl
+{
+    public RatingBar()
+    {
+        InitializeComponent();
+    }
+
+    public static readonly DependencyProperty RatingValueProperty = DependencyProperty.Register(
+    "RatingValue", typeof(int), typeof(RatingBar), new PropertyMetadata(0, OnRatingValueChanged));
+
+    public int RatingValue
+    {
+        get { return (int)GetValue(RatingValueProperty); }
+        set { SetValue(RatingValueProperty, value); }
+    }
+
+    private static void OnRatingValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var control = d as RatingBar;
+        int newRatingValue = (int)e.NewValue;
+        // Update visual state of control here based on newRatingValue
+    }
+
+    private void OnRatingButtonClick(object sender, RoutedEventArgs e)
+    {
+        var button = sender as Button;
+        RatingValue = int.Parse(button.Tag.ToString());
+
+        var parentGrid = button.Parent as Grid;
+
+        // Loop through each child control in the Grid
+        foreach (var child in parentGrid.Children)
+        {
+            // Check if the child control is a Button
+            if (child is Button childButton && int.TryParse(childButton.Tag.ToString(), out int childRatingValue))
+            {
+                // Change the color of each button which has the same or lesser value of what's being hovered or clicked on
+                if (childRatingValue <= RatingValue)
+                {
+                    childButton.Foreground = (SolidColorBrush)FindResource("ThirnaryBrush");
+                }
+                else
+                {
+                    childButton.Foreground = (SolidColorBrush)FindResource("ThirnaryMidBrush");
+                }
+            }
+        }
+    }
+}
