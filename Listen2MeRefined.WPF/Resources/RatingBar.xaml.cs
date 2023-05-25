b6bf86a3ae3859ff5d0fc3e429aa516846bf8bfb
György Turbék <profgyuri@gmail.com>
2023-05-26 01:26:55 +0200

    Added rating bar preview value highlighting

--- Listen2MeRefined.WPF/Resources/RatingBar.xaml.cs
+++ Listen2MeRefined.WPF/Resources/RatingBar.xaml.cs
@@ -56,4 +56,55 @@ public partial class RatingBar : UserControl
             }
         }
     }
+
+    private void Button_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+    {
+        var button = sender as Button;
+        var previewValue = int.Parse(button.Tag.ToString());
+
+        var parentGrid = button.Parent as Grid;
+
+        // Loop through each child control in the Grid
+        foreach (var child in parentGrid.Children)
+        {
+            // Check if the child control is a Button
+            if (child is Button childButton && int.TryParse(childButton.Tag.ToString(), out int childRatingValue))
+            {
+                // Change the color of each button which has the same or lesser value of what's being hovered or clicked on
+                if (childRatingValue <= previewValue)
+                {
+                    childButton.Foreground = (SolidColorBrush)FindResource("ThirnaryLightBrush");
+                }
+                else
+                {
+                    childButton.Foreground = (SolidColorBrush)FindResource("ThirnaryMidBrush");
+                }
+            }
+        }
+    }
+
+    private void Button_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+    {
+        var button = sender as Button;
+
+        var parentGrid = button.Parent as Grid;
+
+        // Loop through each child control in the Grid
+        foreach (var child in parentGrid.Children)
+        {
+            // Check if the child control is a Button
+            if (child is Button childButton && int.TryParse(childButton.Tag.ToString(), out int childRatingValue))
+            {
+                // Change the color of each button which has the same or lesser value of what's being hovered or clicked on
+                if (childRatingValue <= RatingValue)
+                {
+                    childButton.Foreground = (SolidColorBrush)FindResource("ThirnaryBrush");
+                }
+                else
+                {
+                    childButton.Foreground = (SolidColorBrush)FindResource("ThirnaryMidBrush");
+                }
+            }
+        }
+    }
 }
