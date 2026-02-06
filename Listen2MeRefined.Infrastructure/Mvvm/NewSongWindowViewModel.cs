afac2be9894a0dd33776dcb72bd4cfe4b1ee88ac
György Turbék <profgyuri@gmail.com>
2026-02-06 02:13:12 +0100

    Add better logging to NewSongWindowViewModel

--- Listen2MeRefined.Infrastructure/Mvvm/NewSongWindowViewModel.cs
+++ Listen2MeRefined.Infrastructure/Mvvm/NewSongWindowViewModel.cs
@@ -21,23 +21,30 @@ public sealed partial class NewSongWindowViewModel :
     };
 
     private readonly ISettingsManager<AppSettings> _settingsManager;
+    private readonly ILogger _logger;
 
     public bool IsTopmost { get; set; }
 
-    public NewSongWindowViewModel(ISettingsManager<AppSettings> settingsManager)
+    public NewSongWindowViewModel(ISettingsManager<AppSettings> settingsManager, ILogger logger)
     {
         _settingsManager = settingsManager;
+        _logger = logger;
+
+        _logger.Debug("[NewSongWindowViewModel] initialized");
     }
 
     protected override Task InitializeCoreAsync(CancellationToken ct)
     {
         IsTopmost = _settingsManager.Settings.NewSongWindowPosition == "Always on top";
 
+        _logger.Debug("[NewSongWindowViewModel] Finished InitializeCoreAsync");
+
         return base.InitializeCoreAsync(ct);
     }
 
     public Task Handle(NewSongWindowPositionChangedNotification notification, CancellationToken cancellationToken)
     {
+        _logger.Information("[NewSongWindowViewModel] Received NewSongWindowPositionChangedNotification: {Position}", notification.Position);
         IsTopmost = notification.Position == "Always on top";
         OnPropertyChanged(nameof(IsTopmost));
         return Task.CompletedTask;
@@ -45,6 +52,7 @@ public sealed partial class NewSongWindowViewModel :
 
     public Task Handle(CurrentSongNotification notification, CancellationToken cancellationToken)
     {
+        _logger.Information("[NewSongWindowViewModel] Received CurrentSongNotification: {@Audio}", notification.Audio);
         Song = notification.Audio;
         return Task.CompletedTask;
     }
