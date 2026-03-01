d5579a7d2772adbe489fc633f38dbd4f17e11cf8
György Turbék <profgyuri@gmail.com>
2026-03-01 23:43:19 +0100

    Add migration for `MutedDroppedSongFoldersJson` column in `Settings` table.

---
+++ Listen2MeRefined.Infrastructure/Migrations/20260301224203_PendingChanges.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Listen2MeRefined.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class PendingChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MutedDroppedSongFoldersJson",
+                table: "Settings",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MutedDroppedSongFoldersJson",
+                table: "Settings");
+        }
+    }
+}
