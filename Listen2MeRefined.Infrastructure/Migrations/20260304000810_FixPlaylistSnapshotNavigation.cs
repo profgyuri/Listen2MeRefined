008841dee9fb53799c1f7810ceeb916dd30c44eb
György Turbék <profgyuri@gmail.com>
2026-03-04 01:17:31 +0100

    Fix PlaylistSnapshot navigation issues and update EF Core migration snapshot.

---
+++ Listen2MeRefined.Infrastructure/Migrations/20260304000810_FixPlaylistSnapshotNavigation.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Listen2MeRefined.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPlaylistSnapshotNavigation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
