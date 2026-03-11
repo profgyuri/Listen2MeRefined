f3cb335cc138394235117d9cb58fd29646d70018
György Turbék <profgyuri@gmail.com>
2026-03-11 02:01:04 +0100

    Moved `RelayCommand` to `Utils` namespace, removed duplicates from `MainWindow.xaml.cs` and `SettingsWindow.xaml.cs`, and updated project file accordingly.

--- Listen2MeRefined.WPF/MainWindow.xaml.cs
+++ Listen2MeRefined.WPF/MainWindow.xaml.cs
@@ -39,27 +39,4 @@ public sealed partial class MainWindow : Window
         _globalHook.Unregister();
         System.Windows.Application.Current.Shutdown();
     }
-
-    private sealed class RelayCommand : ICommand
-    {
-        private readonly Action<object?> _execute;
-
-        public RelayCommand(Action<object?> execute)
-        {
-            _execute = execute;
-        }
-
-        public bool CanExecute(object? parameter) => true;
-
-        public void Execute(object? parameter)
-        {
-            _execute(parameter);
-        }
-
-        public event EventHandler? CanExecuteChanged
-        {
-            add { }
-            remove { }
-        }
-    }
 }
--- Listen2MeRefined.WPF/Views/SettingsWindow.xaml.cs
+++ Listen2MeRefined.WPF/Views/SettingsWindow.xaml.cs
@@ -60,27 +60,4 @@ public sealed partial class SettingsWindow : Window
     {
         Hide();
     }
-
-    private sealed class RelayCommand : ICommand
-    {
-        private readonly Action<object?> _execute;
-
-        public RelayCommand(Action<object?> execute)
-        {
-            _execute = execute;
-        }
-
-        public bool CanExecute(object? parameter) => true;
-
-        public void Execute(object? parameter)
-        {
-            _execute(parameter);
-        }
-
-        public event EventHandler? CanExecuteChanged
-        {
-            add { }
-            remove { }
-        }
-    }
 }
