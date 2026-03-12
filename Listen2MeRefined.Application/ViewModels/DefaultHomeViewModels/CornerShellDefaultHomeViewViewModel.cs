e81b346c5d58f420b824fdc09ab0ff3b91626083
György Turbék <profgyuri@gmail.com>
2026-03-12 03:07:53 +0100

    Added shell views and corresponding view models for all windows. Updated `App.xaml` with data templates for new views.

---
+++ Listen2MeRefined.Application/ViewModels/DefaultHomeViewModels/CornerShellDefaultHomeViewViewModel.cs
@@ -0,0 +1,15 @@
+using CommunityToolkit.Mvvm.Messaging;
+using Listen2MeRefined.Application.ErrorHandling;
+using Serilog;
+
+namespace Listen2MeRefined.Application.ViewModels.DefaultHomeViewModels;
+
+public class CornerShellDefaultHomeViewViewModel : ViewModelBase
+{
+    public CornerShellDefaultHomeViewViewModel(
+        IErrorHandler errorHandler, 
+        ILogger logger, 
+        IMessenger messenger) : base(errorHandler, logger, messenger)
+    {
+    }
+}
---
+++ Listen2MeRefined.WPF/Views/DefaultHomeViews/SettingsShellDefaultHomeView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace Listen2MeRefined.WPF.Views.DefaultHomeViews;
+
+public partial class SettingsShellDefaultHomeView : UserControl
+{
+    public SettingsShellDefaultHomeView()
+    {
+        InitializeComponent();
+    }
+}
