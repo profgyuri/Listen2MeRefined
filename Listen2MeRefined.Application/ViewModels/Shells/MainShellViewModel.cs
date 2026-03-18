5dcdf22c9c8de9c6ad3b4a94b2474af9761932f9
György Turbék <profgyuri@gmail.com>
2026-03-18 02:50:33 +0100

    Implemented `IAppUpdateChecker` in `MainShellViewModel` to detect updates and updated XAML binding to use `IsUpdateAvailableFeedbackVisible` for feedback visibility management.

--- Listen2MeRefined.Application/ViewModels/Shells/MainShellViewModel.cs
+++ Listen2MeRefined.Application/ViewModels/Shells/MainShellViewModel.cs
@@ -1,9 +1,11 @@
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using Listen2MeRefined.Application.ErrorHandling;
 using Listen2MeRefined.Application.Navigation;
 using Listen2MeRefined.Application.Navigation.Windows;
 using Listen2MeRefined.Application.Startup;
+using Listen2MeRefined.Application.Updating;
 using Microsoft.Extensions.Options;
 using Serilog;
 
@@ -14,6 +16,9 @@ public sealed partial class MainShellViewModel : ShellViewModelBase
     private readonly NavigationOptions _navigationOptions;
     private readonly IWindowManager _windowManager;
     private readonly IStartupManager _startupManager;
+    private readonly IAppUpdateChecker _appUpdateChecker;
+    
+    [ObservableProperty] private bool _isUpdateAvailable;
     
     public MainShellViewModel(
         IErrorHandler errorHandler, 
@@ -22,16 +27,20 @@ public sealed partial class MainShellViewModel : ShellViewModelBase
         IShellContextFactory context, 
         IOptions<NavigationOptions> navigationOptions, 
         IWindowManager windowManager, 
-        IStartupManager startupManager) : base(errorHandler, logger, messenger, context.Create())
+        IStartupManager startupManager, 
+        IAppUpdateChecker appUpdateChecker) : base(errorHandler, logger, messenger, context.Create())
     {
         _windowManager = windowManager;
         _startupManager = startupManager;
+        _appUpdateChecker = appUpdateChecker;
         _navigationOptions = navigationOptions.Value;
     }
 
     public override async Task InitializeAsync(CancellationToken cancellationToken = default)
     {
         await _startupManager.StartAsync(cancellationToken);
+
+        IsUpdateAvailable = (await _appUpdateChecker.CheckForUpdatesAsync()).IsUpdateAvailable;
         
         await NavigationService
             .NavigateAsync(_navigationOptions.DefaultRoute, cancellationToken: cancellationToken)
